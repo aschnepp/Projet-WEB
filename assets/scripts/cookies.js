4f8e2a5e3ee33deebf2b9119fccb3ebb2afe7789
Red-Hide <guigui67480@gmail.com>
2024-04-05 00:30:25 +0200

    Made cookies use absolute path

--- assets/scripts/cookies.js
+++ assets/scripts/cookies.js
@@ -4,13 +4,13 @@ function submitForm(event) {
   var formData = new FormData(document.getElementById("myForm"));
 
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "../controller/controllerLogin.php", true);
+  xhr.open("POST", "/controller/controllerLogin.php", true);
   xhr.send(formData);
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        window.location.href = "../index.php";
+        window.location.href = "/index.php";
       } else {
         console.log(xhr.responseText);
         alert("Connexion échouée");
